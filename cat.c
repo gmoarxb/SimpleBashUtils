cdbaf75ceb03aa323e49a109f08a3a948a792799
Power Brandie <powerbra@student.21-school.ru>
2023-10-24 22:22:28 +0300

    file opening

--- cat.c
+++ cat.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #define OPTIONS_END -1
+#define FILE_READ_MODE "r"
 
 static const char SHORT_OPTIONS[] = "AbeEnstTuv";
 
@@ -28,35 +29,37 @@ struct Options {
 };
 typedef struct Options Options;
 
-static void set_option(const char option, Options *options);
+static void init_options(int argc, char* const argv[], Options* options);
+static void set_option(const char option, Options* options);
 static void print_help();
 static void print_invalid_option();
 
-int main(int argc, char *argv[]) {
-  Options options = {0};
+static void process_files(int file_count, char* const file_path[],
+                          const Options* const options);
+static void print_invalid_file(const char* const file_name);
+static void print_file(FILE* file, const Options* const options);
 
+int main(int argc, char* argv[]) {
+  Options options = {0};
+  init_options(argc, argv, &options);
+  process_files(argc - optind, argv + optind, &options);
+  return EXIT_SUCCESS;
+}
+
+static void init_options(int argc, char* const argv[], Options* options) {
   int long_options_index = 0;
 
   char current_option =
       getopt_long(argc, argv, SHORT_OPTIONS, LONG_OPTIONS, &long_options_index);
 
   while (current_option != OPTIONS_END) {
-    set_option(current_option, &options);
+    set_option(current_option, options);
     current_option = getopt_long(argc, argv, SHORT_OPTIONS, LONG_OPTIONS,
                                  &long_options_index);
   }
-
-  printf("b - %d\ne - %d\nn - %d\ns - %d\nt - %d\nu - %d\nv - %d\n", options.b,
-         options.e, options.n, options.s, options.t, options.u, options.v);
-
-  while (optind < argc) {
-    puts(argv[optind++]);
-  }
-
-  return EXIT_SUCCESS;
 }
 
-static void set_option(const char option, Options *options) {
+static void set_option(const char option, Options* options) {
   switch (option) {
     case 'A':
       options->v = true;
@@ -65,7 +68,6 @@ static void set_option(const char option, Options *options) {
       break;
     case 'b':
       options->b = true;
-      options->n = false;
       break;
     case 'e':
       options->v = true;
@@ -106,10 +108,39 @@ static void print_help() {
   fprintf(stderr,
           "Usage: cat [OPTION]... [FILE]...\nConcatenate FILE(s) to standard "
           "output.\n");
+
   exit(EXIT_FAILURE);
 }
 
 static void print_invalid_option() {
   fprintf(stderr, "Try 'cat --help' for more information.\n");
+
   exit(EXIT_FAILURE);
+}
+
+static void process_files(int file_count, char* const file_path[],
+                          const Options* const options) {
+  FILE* current_file = NULL;
+  while (file_count > 0) {
+    current_file = fopen(*file_path, FILE_READ_MODE);
+    if (current_file == NULL) {
+      print_invalid_file(*file_path);
+    } else {
+      print_file(current_file, options);
+    }
+    ++file_path;
+    --file_count;
+  }
+}
+
+static void print_invalid_file(const char* const file_name) {
+  fprintf(stderr, "%s: No such file or directory\n", file_name);
+}
+
+static void print_file(FILE* file, const Options* const options) {
+  if (options) {
+    puts("We Are Here!");
+  }
+  putc(fgetc(file), stdout);
+  puts("");
 }
