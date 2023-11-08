ea2fb81865065ba8ef311c3eebfcacc3a11735d4
Power Brandie <powerbra@student.21-school.ru>
2023-11-08 13:11:23 +0300

    safe fopen, malloc, realloc and short varnames style

--- grep/grep.c
+++ grep/grep.c
@@ -4,30 +4,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GETOPT_OPTIONS_END -1
-#define FOPEN_READ_MODE "r"
+#define GETOPT_END -1
+#define FOPEN_READ "r"
 
-#define PATTERNS_INIT_CAPACITY 128
-#define PATTERNS_CAPACITY_ADDEND 128
+#define PATTERNS_INIT 128
+#define PATTERNS_ADD 128
 
-#define FILE_BUFFER_INIT_CAPACITY 128
-#define FILE_BUFFER_CAPACITY_MULTIPLIER 2
+#define BUFFER_INIT 128
+#define BUFFER_MULT 2
 
-static const char SHORT_OPTIONS[] = "e:f:isvnholc";
+static const char SHORTOPTS[] = "e:f:isvnholc";
 
-static const struct option LONG_OPTIONS[] = {
-    {"regexp", 1, NULL, 'e'},
-    {"file", 1, NULL, 'f'},
-    {"ignore-case", 0, NULL, 'i'},
-    {"no-messages", 0, NULL, 's'},
-    {"invert-match", 0, NULL, 'v'},
-    {"line-number", 0, NULL, 'n'},
-    {"no-filename", 0, NULL, 'h'},
-    {"only-matching", 0, NULL, 'o'},
-    {"files-with-matches", 0, NULL, 'l'},
-    {"count", 0, NULL, 'c'},
-    {"help", 0, NULL, 0},
+static const struct option LONGOPTS[] = {
+    {"regexp", required_argument, NULL, 'e'},
+    {"file", required_argument, NULL, 'f'},
+    {"ignore-case", no_argument, NULL, 'i'},
+    {"no-messages", no_argument, NULL, 's'},
+    {"invert-match", no_argument, NULL, 'v'},
+    {"line-number", no_argument, NULL, 'n'},
+    {"no-filename", no_argument, NULL, 'h'},
+    {"only-matching", no_argument, NULL, 'o'},
+    {"files-with-matches", no_argument, NULL, 'l'},
+    {"count", no_argument, NULL, 'c'},
+    {"help", no_argument, NULL, 0},
     {NULL, 0, NULL, 0}};
+
+typedef struct Patterns {
+  char** data;
+  size_t cur_size;
+  size_t max_size;
+} Patterns;
 
 typedef struct Options {
   bool e;
@@ -40,99 +46,106 @@ typedef struct Options {
   bool o;
   bool l;
   bool c;
-  struct {
-    char** data;
-    unsigned size;
-    unsigned capacity;
-  } patterns;
+  Patterns patts;
 } Options;
 
-static void init_options(int argc, char* const argv[], Options* const options);
-static void free_options(Options* const options);
-static void set_option(const char option, Options* const options);
+static void init_options(int argc, char* const argv[], Options* const opts);
+static void init_patterns(Patterns* const patts);
+static void process_files(int file_cnt, char* const file_path[],
+                          const Options* const opts);
+
+static void free_options(Options* const opts);
 static void print_help();
+static void print_invalid_file(const char* const filename);
+
+static void* safe_malloc(const size_t size);
+static void set_option(const char opt, Options* const opts);
+static void add_patterns_from_string(const char* const str,
+                                     Patterns* const patts);
+
+static void add_patterns_from_file(char* const filename, Patterns* const patts);
+static FILE* safe_fopen(const char* filename, const char* modes);
 static void print_invalid_option();
-
-static void add_pattern(const char* const pattern, Options* const options);
-static void add_patterns_from_string(const char* const patterns,
-                                     Options* const options);
-static void add_patterns_from_file(char* const file_path,
-                                   Options* const options);
-
-static void print_invalid_file(const char* const file_name);
-static void get_patterns_from_file(FILE* pattern_file, char* file_buffer);
-
-static void* safe_malloc(size_t size, const char* called_from);
-static char* string_malloc(size_t length);
-static void* safe_realloc(void* ptr, size_t size, const char* called_from);
+static void* safe_realloc(void* ptr, size_t size);
+static void add_pattern(const char* const patt, Patterns* const patts);
+static void get_patterns_from_file(FILE* file, char* buffer);
 
 int main(int argc, char* argv[]) {
-  Options options = {0};
-  init_options(argc, argv, &options);
-  for (unsigned i = 0; i < options.patterns.size; ++i) {
-    puts(options.patterns.data[i]);
+  Options opts = {0};
+  init_options(argc, argv, &opts);
+  // process_files(argc - optind, argv + optind, &opts);
+  for (size_t i = 0; i < opts.patts.cur_size; ++i) {
+    puts(opts.patts.data[i]);
   }
-  free_options(&options);
+  free_options(&opts);
   return 0;
 }
 
-static void init_options(int argc, char* const argv[], Options* const options) {
-  options->patterns.capacity = PATTERNS_INIT_CAPACITY;
-  options->patterns.size = 0;
-  options->patterns.data =
-      safe_malloc(sizeof(char*) * PATTERNS_INIT_CAPACITY,
-                  "void init_options(int, char* const, struct Options* const)");
-
-  int long_options_index = 0;
-  char current_option =
-      getopt_long(argc, argv, SHORT_OPTIONS, LONG_OPTIONS, &long_options_index);
-  while (current_option != GETOPT_OPTIONS_END) {
-    set_option(current_option, options);
-    current_option = getopt_long(argc, argv, SHORT_OPTIONS, LONG_OPTIONS,
-                                 &long_options_index);
+static void init_options(int argc, char* const argv[], Options* const opts) {
+  init_patterns(&opts->patts);
+  int longind = 0;
+  char cur_opt = getopt_long(argc, argv, SHORTOPTS, LONGOPTS, &longind);
+  while (cur_opt != GETOPT_END) {
+    set_option(cur_opt, opts);
+    cur_opt = getopt_long(argc, argv, SHORTOPTS, LONGOPTS, &longind);
   }
 }
 
-static void free_options(Options* const options) {
-  for (unsigned i = 0; i < options->patterns.size; ++i) {
-    free(options->patterns.data[i]);
+static void free_options(Options* const opts) {
+  for (size_t i = 0; i < opts->patts.cur_size; ++i) {
+    free(opts->patts.data[i]);
   }
-  free(options->patterns.data);
+  free(opts->patts.data);
 }
 
-static void set_option(const char option, Options* const options) {
-  switch (option) {
+static void init_patterns(Patterns* const patts) {
+  patts->cur_size = 0;
+  patts->max_size = PATTERNS_INIT;
+  patts->data = safe_malloc(sizeof(char*) * patts->max_size);
+}
+
+static void* safe_malloc(const size_t size) {
+  void* ret_ptr = malloc(size);
+  if (ret_ptr == NULL) {
+    fprintf(stderr, "Memory allocation error.\n");
+    exit(EXIT_FAILURE);
+  }
+  return ret_ptr;
+}
+
+static void set_option(const char opt, Options* const opts) {
+  switch (opt) {
     case 'e':
-      options->e = true;
-      add_patterns_from_string(optarg, options);
+      opts->e = true;
+      add_patterns_from_string(optarg, &opts->patts);
       break;
     case 'f':
-      options->f = true;
-      add_patterns_from_file(optarg, options);
+      opts->f = true;
+      add_patterns_from_file(optarg, &opts->patts);
       break;
     case 'i':
-      options->i = true;
+      opts->i = true;
       break;
     case 's':
-      options->s = true;
+      opts->s = true;
       break;
     case 'v':
-      options->v = true;
+      opts->v = true;
       break;
     case 'n':
-      options->n = true;
+      opts->n = true;
       break;
     case 'h':
-      options->h = true;
+      opts->h = true;
       break;
     case 'o':
-      options->o = true;
+      opts->o = true;
       break;
     case 'l':
-      options->l = true;
+      opts->l = true;
       break;
     case 'c':
-      options->c = true;
+      opts->c = true;
       break;
     case 0:
       print_help();
@@ -155,88 +168,72 @@ static void print_invalid_option() {
   exit(EXIT_FAILURE);
 }
 
-static void* safe_malloc(size_t size, const char* called_from) {
-  void* result = malloc(size);
-  if (result == NULL) {
-    fprintf(stderr, "Memory allocation error in %s\n", called_from);
-    exit(EXIT_FAILURE);
-  }
-  return result;
-}
-
-static void* safe_realloc(void* ptr, size_t size, const char* called_from) {
-  void* result = realloc(ptr, size);
-  if (result == NULL) {
-    free(ptr);
-    fprintf(stderr, "Memory reallocation error in %s\n", called_from);
-    exit(EXIT_FAILURE);
-  }
-  return result;
-}
-
-static char* string_malloc(size_t length) {
-  return safe_malloc(sizeof(char) * length + sizeof(char),
-                     "char* string_malloc(size_t)");
-}
-
-static void add_patterns_from_string(const char* const patterns,
-                                     Options* const options) {
-  char* temp_patterns = string_malloc(strlen(patterns));
-  strcpy(temp_patterns, patterns);
-  char* token = strtok(temp_patterns, "\n");
+static void add_patterns_from_string(const char* const str,
+                                     Patterns* const patts) {
+  char* temp_patts = safe_malloc(sizeof(char) * strlen(str) + sizeof(char));
+  strcpy(temp_patts, str);
+  char* token = strtok(temp_patts, "\n");
   while (token != NULL) {
-    add_pattern(token, options);
+    add_pattern(token, patts);
     token = strtok(NULL, "\n");
   }
-  free(temp_patterns);
+  free(temp_patts);
 }
 
-static void add_pattern(const char* const pattern, Options* const options) {
-  if (options->patterns.size == options->patterns.capacity) {
-    options->patterns.capacity += PATTERNS_CAPACITY_ADDEND;
-    char** temp = safe_realloc(
-        options->patterns.data, options->patterns.capacity,
-        "void add_pattern(const char* const, struct Options* const)");
-    options->patterns.data = temp;
+static void add_pattern(const char* const patt, Patterns* const patts) {
+  if (patts->cur_size == patts->max_size) {
+    patts->max_size += PATTERNS_ADD;
+    patts->data = safe_realloc(patts->data, patts->max_size);
   }
-  options->patterns.data[options->patterns.size] =
-      string_malloc(strlen(pattern));
-  strcpy(options->patterns.data[options->patterns.size], pattern);
-  ++options->patterns.size;
+  patts->data[patts->cur_size] =
+      safe_malloc(sizeof(char) * strlen(patt) + sizeof(char));
+  strcpy(patts->data[patts->cur_size++], patt);
 }
 
-static void add_patterns_from_file(char* const file_path,
-                                   Options* const options) {
-  FILE* pattern_file = fopen(file_path, FOPEN_READ_MODE);
-  if (pattern_file != NULL) {
-    char* file_buffer = string_malloc(FILE_BUFFER_INIT_CAPACITY);
-    get_patterns_from_file(pattern_file, file_buffer);
-    add_patterns_from_string(file_buffer, options);
-    free(file_buffer);
-  } else {
-    print_invalid_file(file_path);
+static void* safe_realloc(void* ptr, size_t size) {
+  void* ret_ptr = realloc(ptr, size);
+  if (ret_ptr == NULL) {
+    fprintf(stderr, "Memory reallocation error.\n");
+    exit(EXIT_FAILURE);
   }
-  fclose(pattern_file);
+  return ret_ptr;
 }
 
-static void get_patterns_from_file(FILE* pattern_file, char* file_buffer) {
-  unsigned file_buffer_size = 0;
-  unsigned file_buffer_capacity = FILE_BUFFER_INIT_CAPACITY;
-  int symbol = fgetc(pattern_file);
-  while (!feof(pattern_file)) {
-    file_buffer[file_buffer_size] = symbol;
-    ++file_buffer_size;
-    if (file_buffer_size == file_buffer_capacity) {
-      file_buffer_capacity *= FILE_BUFFER_CAPACITY_MULTIPLIER;
-      char* temp = safe_realloc(file_buffer, file_buffer_capacity,
-                                "void get_patterns_from_file(FILE*, char*)");
-      file_buffer = temp;
+static void add_patterns_from_file(char* const filename,
+                                   Patterns* const patts) {
+  FILE* file = safe_fopen(filename, FOPEN_READ);
+  char* buffer = safe_malloc(sizeof(char) * BUFFER_INIT);
+  get_patterns_from_file(file, buffer);
+  add_patterns_from_string(buffer, patts);
+  free(buffer);
+  fclose(file);
+}
+
+static FILE* safe_fopen(const char* filename, const char* modes) {
+  FILE* ret_ptr = fopen(filename, modes);
+  if (ret_ptr == NULL) {
+    print_invalid_file(filename);
+    exit(EXIT_FAILURE);
+  }
+  return ret_ptr;
+}
+
+static void get_patterns_from_file(FILE* file, char* buffer) {
+  unsigned size = 0;
+  unsigned max_size = BUFFER_INIT;
+  int symbol = fgetc(file);
+  while (!feof(file)) {
+    buffer[size++] = symbol;
+    if (size == max_size) {
+      max_size *= BUFFER_MULT;
+      char* temp = safe_realloc(buffer, max_size);
+      buffer = temp;
     }
-    symbol = fgetc(pattern_file);
+    symbol = fgetc(file);
   }
-  file_buffer[file_buffer_size] = '\0';
+  buffer[size] = '\0';
 }
 
-static void print_invalid_file(const char* const file_name) {
-  fprintf(stderr, "grep: %s: No such file or directory\n", file_name);
+static void print_invalid_file(const char* const filename) {
+  fprintf(stderr, "grep: %s: No such file or directory\n", filename);
 }
